f541d2e9ef17b33117446b4e0f839a15ee65a9f1
bhawnaa05 <bhawnabhandari2004@gmail.com>
2026-01-07 23:01:43 +0530

    changes for less loading time

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,15 +6,41 @@ if (!MONGODB_URI) {
   throw new Error("Please define MONGODB_URI in .env.local");
 }
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare global {
+  var mongoose: any;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectMongoDB = async () => {
-  if (mongoose.connection.readyState >= 1) return;
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("MongoDB connected");
+      return mongoose;
+    });
+  }
 
   try {
-    await mongoose.connect(MONGODB_URI);
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection failed", error);
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
   }
+
+  return cached.conn;
 };
 
 export default connectMongoDB;
